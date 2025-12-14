3d44465658611257aa1300eb1850dcb06114021a
DpkShinde <deepak.shinde@financeshastra.com>
2025-12-14 20:37:15 +0530

    cors fixes

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,14 @@ import { checkDbConnection } from './utils/dbHealthCheck'; // Import the new hea
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: 'https://jobmatrix-frontend.vercel.app/', // Allow only your frontend's origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allow all common methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-}));
+app.use(
+  cors({
+    origin: "https://jobmatrix-frontend.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello from backend!');
